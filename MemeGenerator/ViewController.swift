849dfc7a7759385b08a7b0af2a4cd3e677ac1de7
Chris Tran <christopher.huytran@gmail.com>
2016-04-09 09:32:58 -0500

    enable Camera to show with UIImagePickerController

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -11,16 +11,29 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+    }
 
 
-    @IBAction func pickAnImage(sender: UIBarButtonItem) {
+    @IBAction func pickAnImageFromAlbum(sender: UIBarButtonItem) {
         
         let pickerController = UIImagePickerController()
         pickerController.delegate = self
+        pickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(sender: UIBarButtonItem) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = UIImagePickerControllerSourceType.Camera
         self.presentViewController(pickerController, animated: true, completion: nil)
     }
     

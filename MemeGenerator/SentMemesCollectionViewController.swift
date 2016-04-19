8479080066dc46edbb7fa3454e61331b1e43d045
Chris Tran <christopher.huytran@gmail.com>
2016-04-18 22:13:55 -0500

    add adjustFlowLayout to account for orientation changes

--- MemeGenerator/SentMemesCollectionViewController.swift
+++ MemeGenerator/SentMemesCollectionViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class SentMemesCollectionViewController: UICollectionViewController {
     
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
     var memes: [Meme] {
         return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        adjustFlowLayout()
+    }
+    
+    func adjustFlowLayout() {
+        let frameSize = collectionView?.frame.size
+        let space: CGFloat = 1.5
+        let dimension:CGFloat = frameSize!.width >= frameSize!.height ? (frameSize!.width - (5 * space)) / 6.0 :  (frameSize!.width - (2 * space)) / 3.0
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSizeMake(dimension, dimension)
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        adjustFlowLayout()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -26,6 +47,7 @@ class SentMemesCollectionViewController: UICollectionViewController {
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("SentMemeCollectionCell", forIndexPath: indexPath) as! MemeCollectionViewCell
         
+        cell.backgroundColor = UIColor.whiteColor()
         cell.imageView.image = memes[indexPath.item].memedImage
         
         return cell
